02eaeaac4ef20ca4fa660dad7bf9cc39041bad6a
Mylek89 <mylek89@gmail.com>
2022-01-19 19:34:38 +0000

    first commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Unity_Baselinker', __DIR__);
